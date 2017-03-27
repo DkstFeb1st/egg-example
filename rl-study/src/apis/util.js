fa3e79bc97d37cc58442dbad7e889157f133b411
jinchaoxiang <702290433@qq.com>
2017-03-27 10:29:10 +0800

    api增加模块名

--- rl-study/src/apis/util.js
+++ rl-study/src/apis/util.js
@@ -4,4 +4,5 @@
 const debug = process.env.NODE_ENV !== 'production'
 let $_SERVER = debug ? "http://" : "https://";
 let $_DOMAIN = debug ? "localhost:7001/" : "app.rarcbank.com/";
-export const HEADURL = $_SERVER + $_DOMAIN;
+let $_MODULE = debug ? "" : "study/"
+export const HEADURL = $_SERVER + $_DOMAIN + $_MODULE;
