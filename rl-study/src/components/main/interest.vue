69136b5ff04cb14a1b000c69ff0723ed9f472d9c
jinchaoxiang <702290433@qq.com>
2017-05-15 17:15:04 +0800

    兴趣课列表样式bug

--- rl-study/src/components/main/interest.vue
+++ rl-study/src/components/main/interest.vue
@@ -62,11 +62,6 @@
     <div class="list-wrapper">
 
       <div class="list-item-wrapper" v-for="( item, index ) in interestList " v-on:click="goToDetail(item.id)">
-        <div class="list-item-img">
-          <img
-            :src="item.avator"
-          />
-        </div>
         <div class="list-item-content">
           <p class="list-item-title">{{item.title}}</p>
           <p class="list-item-type">
@@ -77,6 +72,11 @@
                            {{item.view_num}}人观看 | {{item.comment_num}}评论
                         </span>
           </p>
+        </div>
+        <div class="list-item-img">
+          <img
+            :src="item.avator"
+          />
         </div>
       </div>
     </div>
